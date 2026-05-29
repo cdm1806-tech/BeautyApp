8982e386e6bff311eb495ccf4d23279e7aac379f
cdm1806-tech <chongoijohn@gmail.com>
2026-05-29 22:08:00 +0300

    added: beauty-site/vite.config.js

---
+++ beauty-site/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import Services from "./pages/Services"
+import Gallery from "./pages/Gallery"
+import Booking from "./pages/Booking"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ beauty-site/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-6xl font-bold">
+        Beauty Studio
+      </h1>
+    </div>
+  )
+}
+
+export default Home
